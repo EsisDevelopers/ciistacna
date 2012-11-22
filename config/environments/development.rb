cd2149f4e3a77ea43336f0f7b3fe2b006065aeac
moises <elnaufrago2009@hotmail.com>
2012-11-22 11:08:25 -0500

    moises

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,8 @@ Ciistacna::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
+  # Print deprecation notices to the Rails logger :log
+  config.active_support.deprecation = :silenced
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
