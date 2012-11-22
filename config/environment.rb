aad0976c4bb866bc2493dc065097db0fcdc3359b
moises <elnaufrago2009@hotmail.com>
2012-11-22 11:18:54 -0500

    moises

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActiveSupport::Deprecation.silenced = true
 # Initialize the rails application
 Ciistacna::Application.initialize!
 
-ActiveSupport::Deprecation.silenced = true
+
