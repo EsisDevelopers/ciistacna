4cfa01f3930f59df0643be318f4be49caa1af598
moises <elnaufrago2009@hotmail.com>
2012-11-22 12:15:38 -0500

    moises

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,13 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require 'rails/all'
 
-ActiveSupport::Deprecation.silenced = true
+# Silence deprecation warnings until Heroku stops injecting plugins into
+# vendor/plugins
+if Rails.env.production?
+  ActiveSupport::Deprecation.silenced = true
+end
 
 # Initialize the rails application
 Ciistacna::Application.initialize!
